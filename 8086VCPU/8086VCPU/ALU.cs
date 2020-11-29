028ab329d24c528f526e8d260c29e315262a977d
Jonathan Eduardo Garcia Garcia <jonyjouna@gmail.com>
2020-11-28 22:22:00 -0600

    Agregadas funciones de la ALU: Suma,AND,OR y un método para imprimir :) [REUNIÓN]

---
+++ 8086VCPU/8086VCPU/ALU.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8086VCPU
+{
+    public class ALU
+    {
+        const int Bits = 4;
+
+        public int[] Operador1 = new int[Bits];
+        public int[] Operador2 = new int[Bits];
+
+        public int[] Resultado = new int[Bits];
+        public int Acarreo;
+        public void SetOperadores(int[] Op1, int[] Op2)
+        {
+            Operador1 = Op1;
+            Operador2 = Op2;
+        }
+        public void Imprimir()
+        {
+            Console.Write(Acarreo);
+            Console.Write("-");
+            for (int i = 0; i < Bits; i++)
+            {
+                Console.Write(Resultado[i]);
+                Console.Write(",");
+            }
+            Console.Write("\n");
+        }
+
+        public void ADD()
+        {
+            for (int i = Bits - 1; i >= 0; i--)
+            {
+                Resultado[i] = Operador1[i] + Operador2[i] + Acarreo;
+                Acarreo = 0;
+                if (Resultado[i] == 2)
+                {
+                    Resultado[i] = 0;
+                    Acarreo = 1;
+                }
+                else if (Resultado[i] == 3)
+                {
+                    Resultado[i] = 1;
+                    Acarreo = 1;
+                }
+            }
+        }
+        public void AND()
+        {
+            Acarreo = 0;
+            for (int i = Bits - 1; i >= 0; i--)
+            {
+                if (Operador1[i] == 1 && Operador2[i] == 1)
+                {
+                    Resultado[i] = 1;
+                }
+                else
+                {
+                    Resultado[i] = 0;
+                }
+            }
+        }
+        public void OR()
+        {
+            Acarreo = 0;
+            for (int i = Bits - 1; i >= 0; i--)
+            {
+                if (Operador1[i] == 0 && Operador2[i] == 0)
+                {
+                    Resultado[i] = 0;
+                }
+                else
+                {
+                    Resultado[i] = 1;
+                }
+            }
+        }
+
+    }
+}
---
+++ 8086VCPU/Pruebas/Program.cs
@@ -0,0 +1,28 @@
+﻿using _8086VCPU;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ALU alu = new ALU();
+            alu.SetOperadores(new int[] { 1, 0, 0, 1 }, new int[] { 1, 1, 1, 1 });
+            alu.ADD();
+            alu.Imprimir();
+            alu.AND();
+            alu.Imprimir();
+            alu.OR();
+            alu.Imprimir();
+
+            Console.ReadKey(); //Leer una tecla para que no se cierre la consola
+
+      
+        }
+    }
+}
